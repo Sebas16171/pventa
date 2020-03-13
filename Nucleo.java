a3ef24adf30c9356a6eb3f03edfeecd9018723c9
Sebas Cervera <secervera@hotmail.com>
2020-03-12 21:21:53 -0600

    SELECT_FROM-beta

--- Nucleo.java
+++ Nucleo.java
@@ -1,42 +1,58 @@
 import java.util.*;
 import java.sql.*;
 
+class Nucleo {
 
-class Nucleo{
-    
     public static List<Proveedor> lst_Proveedores = new ArrayList<Proveedor>();
     public static List<Cliente> lst_Clientes = new ArrayList<Cliente>();
-    
+
     private static Connection con = null;
-    
-    public static Object Execute_Query(String query){
+
+    public static ResultSet Execute_Query(String query) {
         try {
             String myDriver = "org.gjt.mm.mysql.Driver";
             String myUrl = "jdbc:mysql://localhost/pventa";
             Class.forName(myDriver);
             con = DriverManager.getConnection(myUrl, "root", "");
 
-            if (query != ""){
+            if (query != "") {
                 Statement st = con.createStatement();
 
-                if (query.matches("INSERT INTO.*")){
+                if (query.matches("INSERT INTO.*")) {
                     st.executeUpdate(query);
                     return null;
-                } else if (query.matches("SELECT.*")){
+                } else if (query.matches("SELECT.*")) {
                     ResultSet rs = st.executeQuery(query);
                     return rs;
                 }
             }
             con.close();
         } catch (Exception e) {
-            System.out.println("Sin conexion");
-            System.err.println(e.getMessage());
+            return null;
         }
         return null;
     }
 
     public static void main(String[] args) {
         Execute_Query("");
+
+        ResultSet Proveedores = Execute_Query("SELECT * FROM `proveedor`");
+
+        try {
+            while (Proveedores.next()) {
+                int id = Proveedores.getInt("id_proveedor");
+                String nombre = Proveedores.getString("nombre");
+                String rfc = Proveedores.getString("rfc");
+                String telefono = Proveedores.getString("telefono");
+                String correo = Proveedores.getString("correo");
+                boolean activo = Proveedores.getBoolean("activo");
+
+                // print the results
+                System.out.format("%s, %s, %s, %s, %s, %s\n", id, nombre, rfc, telefono, correo, activo);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         Ventana_Principal V = new Ventana_Principal();
     }
 
